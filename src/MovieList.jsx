7567e949e2b32729ba0be8520a53923eb9fb87d5
Mohamed Samy <mohamed.samy.essia@gmail.com>
2024-06-23 15:35:59 +0300

    Upload

---
+++ src/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Movie from "./Movie";
+
+const MovieList = ({ movies }) => {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import StarRating from "./Start";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setmovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={3}
+        onSetRating={setmovieRating}
+        defaultRating={2}
+      />
+      <p>The Rating is {movieRating}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} defaultRating={10} />
+    <Test />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
